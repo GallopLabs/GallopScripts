1b9ab5ba0fb7480f26ecc62e3111d904516ca0c0
Andy Fang <andy@fang.ws>
2014-11-19 15:53:30 -0500

    commit

--- gallop_csv.py
+++ gallop_csv.py
@@ -12,65 +12,82 @@
 import csv
 from collections import defaultdict
 
-data = open('gdata.csv')
-column = defaultdict(list) # each value in each column is appended to a list
 
-with data as f:
-    reader = csv.DictReader(f) # read rows into a dictionary format
-    for row in reader: # read a row as {column1: value1, column2: value2,...}
-        for (k,v) in row.items(): # go over each column name and value
-            column[k].append(v) # append the value into the appropriate list
-                                 # based on column name k
+class Data:
 
+    def __init__(self, file):
 
-campaign_name = column['Campaign Name']
-impressions = column['Impressions']
+        data = open(file)
+        column = defaultdict(list) # each value in each column is appended to a list
 
-def occurances(s, ch):
-    return [i for i, letter in enumerate(s) if letter == ch]
+        with data as f:
+            reader = csv.DictReader(f) # read rows into a dictionary format
+            for row in reader: # read a row as {column1: value1, column2: value2,...}
+                for (k, v) in row.items(): # go over each column name and value
+                    column[k].append(v) # append the value into the appropriate list
+                                         # based on column name k
 
-def parse(device):
-    """
-    Returns a list of all the values needed to organize Gallop data.
-    :param device: this is the name of the device to search for. All other returned values are in the same row as
-    the device.
-    :return: List of companies, campaigners, audiences, impressions
-    """
-    rows = []
-    i = 0
+        self.campaign_name = column['Campaign Name']
+        self.start_date = column['Start Date']
+        self.end_date = column['End Date']
+        self.reach = column['Reach']
 
-    companies = []
-    campaigners = []
-    audiences = []
-    impressions = 0
-    total_dl = 0
-    spent = 0
-
-    for row in campaign_name:
-        audience = row[occurances(row, '-')[1]:occurances(row, '-')[2]].strip(' -') # filters by audience
-        i += 1 # Index of the matching row to audience
-
-        if audience.lower() == device.lower():
-            company = row[:occurances(row, '-')[0]].strip()
-            campaigner = row[occurances(row, '-')[0]:occurances(row, '-')[1]].strip(' -')
-            impressions += float(column['Impressions'][i - 1])
-            # total_dl += int(column['Total Downloads'])
-            spent += float(column['Amount Spent (USD)'][i - 1])
+    def occurences(s, ch):
+        """
+        Gets all the occurences of a string in a string.
+        :param s: The string to look in.
+        :param ch: The string to find.
+        :return: A list of all indexes of occurence (ch) in s.
+        """
+        return [i for i, letter in enumerate(s) if letter == ch]
 
 
+class Device:
 
-            companies.append(company)
-            campaigners.append(campaigner)
-            audiences.append(audience)
+    def __init__(self, data, device):
+            """
+            Returns a list of all the values needed to organize Gallop data.
+            :param device: this is the case insensitive name of the device to search for. All other returned values are in the
+            same
+            row as
+            the device.
+            :return: List of companies, campaigners, audiences, impressions
+            """
 
 
+            self.rows = []
+            i = 0
+            self.companies = []
+            self.campaigners = []
+            self.audiences = []
+            self.impressions = 0
+            self.total_dl = 0
+            self.spent = 0
+
+            for row in data.campaign_name:
+                audience = row[data.occurences(row, '-')[1]:data.occurences(row, '-')[2]].strip(' -') # filters by
+                # audience
+                i += 1 # Index of the matching row to audience
+
+                if audience.lower() == device.lower():
+                    company = row[:data.occurences(row, '-')[0]].strip()
+                    campaigner = row[data.occurences(row, '-')[0]:data.occurences(row, '-')[1]].strip(' -')
+                    self.impressions += float(data.column['Impressions'][i - 1])
+                    # total_dl += int(column['Total Downloads'])
+                    self.spent += float(data.column['Amount Spent (USD)'][i - 1])
+
+                    self.companies.append(company)
+                    self.campaigners.append(campaigner)
+                    self.audiences.append(audience)
+
+            tuple = self.companies, self.campaigners, self.audiences, "{0:.2f}".format(self.impressions), "{0:.2f}".format(self.spent)
+
+            return list(tuple)
 
 
+if __name__ == 'main':
 
-    tuple = companies, campaigners, audiences, "{0:.2f}".format(impressions), "{0:.2f}".format(spent)
+    nyt = Data('gdata.csv')
+    Iphone = Device(nyt, 'IPHONE')
 
-    return list(tuple)
-
-print(parse('IPHONE'))
-
-help(parse)
+    print(Iphone)
